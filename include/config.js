a30521854c5241b45f41972589f6685ab85e0f25
Jankiny <yangzlnn@foxmail.com>
2024-02-22 08:43:34 +0800

    use _config.solon.yml

--- include/config.js
+++ include/config.js
@@ -32,7 +32,7 @@ function checkConfig(hexo) {
         logger.info('=== Checking theme configurations ===');
 
         const siteCfgFile = path.join(hexo.base_dir, '_config.yml');
-        const themeSiteCfg = path.join(hexo.base_dir, '_config.icarus.yml');
+        const themeSiteCfg = path.join(hexo.base_dir, '_config.solon.yml');
         const themeDirCfg = path.join(hexo.theme_dir, '_config.yml');
         const themeCfgPaths = [themeDirCfg, themeSiteCfg].filter(cfgPath => fs.existsSync(cfgPath));
         const themeSiteCfgExample = themeSiteCfg + '.example';
