b10f70e51f82d27ce772f70a45630ac353425b69
Jankiny <yangzlnn@foxmail.com>
2024-03-05 14:36:41 +0800

    Fix: enable close comments

--- layout/common/comment.jsx
+++ layout/common/comment.jsx
@@ -12,15 +12,29 @@ module.exports = class extends Component {
         if (!comment || typeof comment.type !== 'string') {
             return null;
         }
+        const commentColsed = !page.comments;
 
         return <div class="card">
             <div class="card-content">
                 <h3 class="title is-5">{__('article.comments')}</h3>
                 {(() => {
                     try {
-                        let Comment = view.require('comment/' + comment.type);
-                        Comment = Comment.Cacheable ? Comment.Cacheable : Comment;
-                        return <Comment config={config} page={page} helper={helper} comment={comment} />;
+                        if (!commentColsed) {
+                            let Comment = view.require('comment/' + comment.type);
+                            Comment = Comment.Cacheable ? Comment.Cacheable : Comment;
+                            return <Comment config={config} page={page} helper={helper} comment={comment}/>;
+                        } else {
+                            return <Fragment>
+                                <div id="comment-container">
+                                    <div class="gt-container">
+                                        <h3 class="menu-label has-text-centered">{__('article.comments_closed')}</h3>
+                                    </div>
+                                </div>
+                            </Fragment>;
+                        }
+                        // let Comment = view.require('comment/' + comment.type);
+                        // Comment = Comment.Cacheable ? Comment.Cacheable : Comment;
+                        // return <Comment config={config} page={page} helper={helper} comment={comment} />;
                     } catch (e) {
                         logger.w(`Icarus cannot load comment "${comment.type}"`);
                         return null;
